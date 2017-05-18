6993fd715c7ea9c70ce1e7f1f9c7232ca1909c8f
Kevin Skompinski <kevons5252@gmail.com>
2017-05-18 00:37:39 -0400

    Initial Commit

--- Unit Converter/ViewController.m
+++ Unit Converter/ViewController.m
@@ -8,11 +8,64 @@
 
 #import "ViewController.h"
 
+double convertToCelcius(double input){
+    double celciusValue;
+    celciusValue = (input - 32) * (5.0/9.0);
+    return celciusValue;
+}
+
+double convertToKelvin(double input){
+    double kelvinValue;
+    kelvinValue = (input + 459.67) * (5.0/9.0);
+    return kelvinValue;
+}
+
+double convertToRankine(double input){
+    double rankineValue;
+    rankineValue = (input + 459.67);
+    return rankineValue;
+}
+
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *Input;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *Segment;
+
+@property (weak, nonatomic) IBOutlet UILabel *Output;
+
 
 @end
 
 @implementation ViewController
+
+
+- (IBAction)Button:(id)sender {
+    
+    NSMutableString *buf = [NSMutableString new];
+    
+    double userInput = [self.Input.text doubleValue];
+    
+    if(self.Segment.selectedSegmentIndex == 0){
+        double celcius = convertToCelcius(userInput);
+        [buf appendString: [@(celcius) stringValue]];
+        [buf appendString:@"°C"];
+    }
+    else if(self.Segment.selectedSegmentIndex == 1){
+        double kelvin = convertToKelvin(userInput);
+        [buf appendString: [@(kelvin) stringValue]];
+        [buf appendString:@"°K"];
+    }
+    else{
+        double rankine = convertToRankine(userInput);
+        [buf appendString: [@(rankine) stringValue]];
+        [buf appendString:@"°R"];
+    }
+    
+    self.Output.text = buf;
+     
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -23,5 +76,8 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
 
 @end
